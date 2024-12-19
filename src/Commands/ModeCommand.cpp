7b8499d49bb78e811a73d4f11cbbba46cbc79f43
Luke Slater <lslater@3F6.42angouleme.fr>
2024-12-19 14:05:30 +0100

    fixed the parsing for the mode command, just need to send the correct messages and it should be good

--- src/Commands/ModeCommand.cpp
+++ src/Commands/ModeCommand.cpp
@@ -5,28 +5,47 @@
 #include "../../includes/Channel.hpp"
 #include <sys/socket.h>
 
-bool	CheckModeType(std::string tokens)
+int	CheckModeType(std::string tokens)
 {
 	if (tokens[0] == '-')
-		return (true);
-	else 
-		return (false);
+		return (1);
+	else if (tokens[0] == '+')
+		return (2);
+	else
+		return (0);
 }
 
-int	ParseValues(std::vector<std::string> tokens, Server *server, Client *client)
+int	ParseValues(std::vector<std::string> tokens)
 {
-	(void)client;
-	(void)server;
-	std::string msg;
+	std::string mode = "itkol";
+	std::string args;
+
 	if (tokens.size() == 1)
 		return (1);
 	else
 	{
 		if (tokens.size() == 2 && tokens[1][0] == '#')
-		{
-			std::cout << tokens[1] << std::endl;
-			Broadcast(split("Mode " + tokens[1] + " +nt", ' '), server, client);
 			return (1);
+		else		
+		{
+			int modeType = CheckModeType(tokens[2]);
+			if (modeType == 1 || modeType == 2)
+				tokens[2] = tokens[2].substr(1);		
+			if ((modeType == 2 || modeType == 0) && (tokens[2].size() * 2) != tokens.size() - 3 + tokens[2].size() )
+			{
+				std::cout <<"size isnt correct" << std::endl;
+				return (1);
+			}
+			for (size_t j = 0; j < tokens[2].size();j++)
+			{
+				if (mode.find(tokens[2][j]) == std::string::npos)
+				{
+					std::cout << "cant find char" <<std::endl;
+					return (1);
+				}
+			}
+			if (modeType == 1)
+				std::cout << "removing mode" << std::endl;
 		}
 	}
 	return (0);
@@ -52,30 +71,14 @@ void	CheckAndExecMode(int modeIndex, int argsIndex, std::vector<std::string> tok
 
 void ModeCommand(Server &server, Client *operatorClient, std::vector<std::string> tokens)
 {
-	if (ParseValues(tokens, &server, operatorClient))
-			return ;
-	/*bool modeType = false;
-	std::string modes[] = {"l", "k", "o"};
-	std::string modeArgs = tokens[2];
-	size_t i = 0;
-	size_t j = 0;
-
-	(void)server;
+	std::string modes = "itkol";
 	(void)operatorClient;
-	modeType = CheckModeType(tokens[2]);
-	std::cout <<"hello"  << modeType << std::endl;
-	if (modeType)
-	{
-		modeArgs.substr(1);
-		for (; i < sizeof(modeArgs); i++)
-		{
-			for (;j < sizeof(modes)/sizeof(modes[0]); j++)
-			{
-				if (std::string(1, modeArgs[i]) == modes[j])
-					break;
-			}
-			std::cout << tokens[3] <<std::endl;
-			//CheckAndExecMode(j, i, split(tokens));
-		}
-	}*/
+	(void)server;
+
+	if (ParseValues(tokens))
+			return ;
+	std::string modeArgs = tokens[2];
+	//need to send all the individual messages to the client
+	//parsing works normally (checks for the amount of modes and arguments (1 arg for each mode) and we also now woork with + and - (for the - we just need to send the correct messages aswell))
 }
+
